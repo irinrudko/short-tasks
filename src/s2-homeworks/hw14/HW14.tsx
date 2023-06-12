05d6198bad0a0d316217e494a6b2b490afc4b3f9
Iryna Rudzko <irinrudko@gmail.com>
2023-06-12 10:28:57 +0200

    feat: add hw14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -1,26 +1,24 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import s2 from '../../s1-main/App.module.css'
 import s from './HW14.module.css'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedInput'
-import {useSearchParams} from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 
 /*
-* 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
-* 2 - дописать функцию sendQuery в HW14
-* 3 - дописать функцию onChangeText в HW14
-* 4 - сделать стили в соответствии с дизайном
-* 5 - добавить HW14 в HW5/pages/JuniorPlus
-* */
+ * 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
+ * 2 - дописать функцию sendQuery в HW14
+ * 3 - дописать функцию onChangeText в HW14
+ * 4 - сделать стили в соответствии с дизайном
+ * 5 - добавить HW14 в HW5/pages/JuniorPlus
+ * */
 
-const getTechs = (find: string) => {
+const getTechs = (find: string): Promise<AxiosResponse<{ techs: string[] }>> => {
     return axios
-        .get<{ techs: string[] }>(
-            'https://samurai.it-incubator.io/api/3.0/homework/test2',
-            {params: {find}}
-        )
+        .get<{ techs: string[] }>('https://samurai.it-incubator.io/api/3.0/homework/test2', { params: { find } })
         .catch((e) => {
             alert(e.response?.data?.errorText || e.message)
+            return e
         })
 }
 
@@ -32,24 +30,19 @@ const HW14 = () => {
 
     const sendQuery = (value: string) => {
         setLoading(true)
-        getTechs(value)
-            .then((res) => {
-                // делает студент
-
-                // сохранить пришедшие данные
-
-                //
-            })
+        getTechs(value).then((res) => {
+            setTechs(res.data.techs)
+            // сохранить пришедшие данные
+            setLoading(false)
+        })
     }
 
     const onChangeText = (value: string) => {
         setFind(value)
-        // делает студент
+        setSearchParams({ find: value })
 
         // добавить/заменить значение в квери урла
         // setSearchParams(
-
-        //
     }
 
     useEffect(() => {
@@ -58,7 +51,7 @@ const HW14 = () => {
         setFind(params.find || '')
     }, [])
 
-    const mappedTechs = techs.map(t => (
+    const mappedTechs = techs.map((t) => (
         <div key={t} id={'hw14-tech-' + t} className={s.tech}>
             {t}
         </div>
@@ -77,7 +70,7 @@ const HW14 = () => {
                 />
 
                 <div id={'hw14-loading'} className={s.loading}>
-                    {isLoading ? '...ищем' : <br/>}
+                    {isLoading ? '...ищем' : <br />}
                 </div>
 
                 {mappedTechs}
--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -1,9 +1,8 @@
-import React, {DetailedHTMLProps, InputHTMLAttributes, ReactNode, useState} from 'react'
+import React, { DetailedHTMLProps, InputHTMLAttributes, ReactNode, useState } from 'react'
 import SuperInputText from '../../../hw04/common/c1-SuperInputText/SuperInputText'
 
 // тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement>
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута, кроме type
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
@@ -13,37 +12,38 @@ export type SuperDebouncedInputPropsType = Omit<DefaultInputPropsType, 'type'> &
     onEnter?: () => void
     error?: ReactNode
     spanClassName?: string
-} // илм экспортировать тип SuperInputTextPropsType
-    & { // плюс специальный пропс SuperPagination
+} & {
+    // илм экспортировать тип SuperInputTextPropsType
+    // плюс специальный пропс SuperPagination
     onDebouncedChange?: (value: string) => void
 }
 
-const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
-    {
-        onChangeText,
-        onDebouncedChange,
+const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = ({
+    onChangeText,
+    onDebouncedChange,
 
-        ...restProps // все остальные пропсы попадут в объект restProps
-    }
-) => {
+    ...restProps // все остальные пропсы попадут в объект restProps
+}) => {
     const [timerId, setTimerId] = useState<number | undefined>(undefined)
 
     const onChangeTextCallback = (value: string) => {
         onChangeText?.(value)
 
         if (onDebouncedChange) {
+            timerId && clearTimeout(timerId)
+            setTimerId(
+                +setTimeout(() => {
+                    onDebouncedChange(value)
+                }, 1500)
+            )
             // делает студент
-
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
-
             //
         }
     }
 
-    return (
-        <SuperInputText onChangeText={onChangeTextCallback} {...restProps}/>
-    )
+    return <SuperInputText onChangeText={onChangeTextCallback} {...restProps} />
 }
 
 export default SuperDebouncedInput
